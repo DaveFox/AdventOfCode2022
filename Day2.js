44a05d1712a342400a53a6921d885d3686b97afd
DaveFox <davesfox@hotmail.co.uk>
2022-12-02 18:11:02 +0000

    Day 2 part 2

--- Day2.js
+++ Day2.js
@@ -1,21 +1,52 @@
 const fs = require('fs');
 
+// Part 1
 // A & X = Rock
 // B & Y = Paper
 // C & Z = Scissors
 
+// Part 2 
+// X = loss
+// Y = draw
+// Z = win
+
 function runStrategy() {
   const fileInput = fs.readFileSync('./Day2Input.txt', {encoding: 'utf8'});
   let fileInputArray = fileInput.split('\n');
-  let totalScore = 0
+  //let testData = ['A Y', 'B X', 'C Z'];
+  let totalScore1 = 0
+  let totalScore2 = 0
 
   for (let line of fileInputArray) {
-    let result = calculateResult(line[0], line[2]);
-    let score = calculatePoints(result, line[2]);
-    totalScore += score;
+    let result1 = calculateResult(line[0], line[2]);
+    let score1 = calculatePoints(result1, line[2]);
+    totalScore1 += score1;
+
+    let newHand = getHand(line[0], line[2]);
+    let result2 = calculateResult(line[0], newHand);
+    let score2 = calculatePoints(result2, newHand);
+    totalScore2 += score2;
   }
 
-  console.log('Total Score:', totalScore);
+  console.log('Total Score (strat 1):', totalScore1);
+  console.log('Total Score (strat 2):', totalScore2);
+}
+
+function getHand(opponentMove, guide) {
+  let hand;
+  switch (opponentMove) {
+    case 'A':
+      hand = guide === 'X' ? 'Z' : guide === 'Y' ? 'X' : 'Y';
+      break;
+    case 'B':
+      hand = guide === 'X' ? 'X' : guide === 'Y' ? 'Y' : 'Z';
+      break;
+    case 'C':
+      hand = guide === 'X' ? 'Y' : guide === 'Y' ? 'Z' : 'X';
+      break;
+  }
+
+  return hand;
 }
 
 function calculateResult(opponent, mine) {

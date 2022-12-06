ed16312ea0fc322bdee74137919d756168e2f9f4
DaveFox <davesfox@hotmail.co.uk>
2022-12-06 21:56:55 +0000

    Day 6 part 2

--- Day6.js
+++ Day6.js
@@ -5,11 +5,13 @@ function main() {
   const fileInputString = fileInput.split('\n')[0];
   const testInput1 = ['mjqjpqmgbljsphdztnvjfqwrcgsmlb'][0];
   const testInput2 = ['bvwbjplbgvbhsrlpgdmjqwftvncz'][0];
-  const testInput = ['zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw'][0];
+  const testInput5 = ['zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw'][0];
 
+  const input = fileInputString;
+  
   let foundPostion = 0;
-  for (let i=3; i<fileInputString.length; i++) {
-    let substring = fileInputString.slice(i - 3, i) + fileInputString[i]
+  for (let i=3; i<input.length; i++) {
+    let substring = input.slice(i - 3, i) + input[i]
     let comparison = '';
     let duplicate = false;
 
@@ -26,6 +28,26 @@ function main() {
     }
   }
   console.log('First marker position:', foundPostion + 1);
+
+  let foundMessagePostion = 0;
+  for (let i=13; i<input.length; i++) {
+    let substring = input.slice(i - 13, i) + input[i]
+    let comparison = '';
+    let duplicate = false;
+
+    for (let j=0; j<substring.length; j++) {
+      if(comparison.includes(substring[j])) {
+        duplicate = true;
+      }
+      comparison += substring[j];
+    }
+
+    if(!duplicate) {
+      foundMessagePostion = i
+      break;
+    }
+  }
+  console.log('First message position:', foundMessagePostion + 1);
 }
 
 main();

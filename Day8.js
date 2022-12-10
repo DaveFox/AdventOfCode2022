c3d6ee6cb5b47b6cb61b7bdfd2ddc2e901e9eef0
DaveFox <davesfox@hotmail.co.uk>
2022-12-10 19:23:15 +0000

    Day 8 part 2

--- Day8.js
+++ Day8.js
@@ -13,6 +13,7 @@ function main() {
 
   const edgeNumber = (heightmap.length * 4) - 4;
   let numberTreesVisible = edgeNumber;
+  let bestScore = 0;
 
   for (let i=1; i < heightmap.length-1; i++) {
     for (let j=1; j < heightmap[i].length-1; j++) {
@@ -25,10 +26,14 @@ function main() {
       //console.log(tree, visibleTop, visibleRight, visibleBottom, visibleLeft, [i,j]);
 
       if(visibleTop || visibleRight || visibleBottom || visibleLeft) numberTreesVisible++;
+
+      const scenicScore = getScore(tree, i, j, heightmap);
+      bestScore = (scenicScore > bestScore) ? scenicScore : bestScore;
     }
   }
 
   console.log('Number of trees visible:', numberTreesVisible);
+  console.log('Highest senic score:', bestScore);
 }
 
 function checkTop(tree, row, col, heightMap) {
@@ -61,6 +66,63 @@ function checkLeft(tree, col, row) {
     if (row[i] >= tree) visible = false;
   }
   return visible;
+}
+
+function getScore(treeHeight, rowNum, colNum, heightmap) {
+  const top = scoreTop(treeHeight, rowNum, colNum, heightmap);
+  const bottom = scoreBottom(treeHeight, rowNum, colNum, heightmap);
+  const right = scoreRight(treeHeight, colNum, heightmap[rowNum]);
+  const left = scoreLeft(treeHeight, colNum, heightmap[rowNum]);
+  // console.log('tree', treeHeight, 'scores:', top, right, bottom, left);
+  return top * right * bottom * left;
+}
+
+function scoreTop(tree, row, col, heightMap) {
+  let score = 0;
+  for (i=row-1; i >= 0; i--) {
+    if (heightMap[i][col] >= tree){
+      score++;
+      break;
+    }
+    score++;
+  }
+  return score;
+}
+
+function scoreBottom(tree, row, col, heightMap) {
+  let score = 0;
+  for (i=row+1; i < heightMap.length; i++) {
+    if (heightMap[i][col] >= tree) {
+      score++;
+      break
+    }
+    score++;
+  }
+  return score;
+}
+
+function scoreRight(tree, col, row) {
+  let score = 0;
+  for (i=col+1; i < row.length; i++) {
+    if (row[i] >= tree) {
+      score++;
+      break;
+    };
+    score++;
+  }
+  return score;
+}
+
+function scoreLeft(tree, col, row) {
+  let score = 0;
+  for (i=col-1; i>=0; i--) {
+    if (row[i] >= tree) {
+      score++;
+      break;
+    };
+    score++;
+  }
+  return score;
 }
 
 main();

2799a6673f5ce33c18f016dd480f4c5d0aadea5a
David Fox <david.fox@ge.com>
2022-12-12 19:57:33 +0000

    Day 9 part 2

--- Day9.js
+++ Day9.js
@@ -3,7 +3,8 @@ const fs = require('fs');
 function main() {
   const fileInput = fs.readFileSync('./Day9Input.txt', {encoding: 'utf8'});
   const fileInputString = fileInput.split('\n');
-  const testInput = ['R 4', 'U 4', 'L 3', 'D 1', 'R 4', 'D 1', 'L 5', 'R 2']
+  const testInput = ['R 4', 'U 4', 'L 3', 'D 1', 'R 4', 'D 1', 'L 5', 'R 2'];
+  const testInputLong = ['R 5', 'U 8', 'L 8', 'D 3', 'R 17', 'D 10', 'L 25', 'U 20'];
 
   // Grid for dev ref
   // 0,2 1,2 2,2
@@ -11,34 +12,35 @@ function main() {
   // 0,0 1,0 2,0
 
   const head = {
-    start: [0,0],
     position: [0,0],
   };
 
   const tail = {
-    start: [0,0],
     position: [0,0],
     visited: [[0,0]]
   };
 
+  const knots = []
+  const ropeLength = 9;
+  for (let i=0; i < ropeLength; i++) {
+    knots.push({
+      position: [0,0],
+      visited: [[0,0]]
+    })
+  }
+
   for(const line of fileInputString) {
     const instruction = line.split(' ')[0];
     const distance = Number(line.split(' ')[1]);
-    if(instruction === 'U') headMoveUp(distance, head, tail);
-    if(instruction === 'D') headMoveDown(distance, head, tail);
-    if(instruction === 'R') headMoveRight(distance, head, tail)
-    if(instruction === 'L') headMoveLeft(distance, head, tail)
+    if(instruction === 'U') headMoveUp(distance, head, tail, knots);
+    if(instruction === 'D') headMoveDown(distance, head, tail, knots);
+    if(instruction === 'R') headMoveRight(distance, head, tail, knots);
+    if(instruction === 'L') headMoveLeft(distance, head, tail, knots);
   }
 
   console.log('Tail visited unique positions:', tail.visited.length);
-}
-
-function sameColumn(pos1, pos2) {
-  return (pos1[0] === pos2[0]);
-}
-
-function sameRow(pos1, pos2) {
-  return (pos1[1] === pos2[1]);
+  console.log('Knot tail visited unique positions:', knots[8].visited.length);
+  // drawGrid(head, knots);
 }
 
 function tailNotVisited(tail) {
@@ -49,58 +51,129 @@ function tailNotVisited(tail) {
   return hasVisited;
 }
 
-function headMoveUp(distance, head, tail) {
+function drawGrid(head, knots) {
+  console.log('');
+  for(let i=15; i>=-10; i--) {
+    let rowStr = '';
+    for(let j=-15; j<20; j++) {
+      let add = '.'
+      if(head.position[0] === j && head.position[1] === i) {
+        add = 'H';
+      }
+      for(const [index, knot] of knots.entries()) {
+        if(knot.position[0] === j && knot.position[1] === i) {
+          if(add !== 'H') add = `${index + 1}`
+        }
+      }
+      if (i === 0 && j === 0) {
+        add = 'S';
+      }
+      rowStr += add;
+    }
+    console.log(rowStr);
+  }
+}
+
+function moveThingUpDown(leadingThing, thing, plusMinus) {
+  if (Math.abs(thing.position[1] - leadingThing.position[1]) >= 2) {
+    if (leadingThing.position[0] > thing.position[0]) thing.position[0]++;
+    if (leadingThing.position[0] < thing.position[0]) thing.position[0]--;
+    if (leadingThing.position[1] > thing.position[1]) thing.position[1]++;
+    if (leadingThing.position[1] < thing.position[1]) thing.position[1]--;
+
+    if(tailNotVisited(thing)) thing.visited.push([thing.position[0], thing.position[1]]);
+    return;
+  }
+
+  if (Math.abs(thing.position[0] - leadingThing.position[0]) >= 2) {
+    if (leadingThing.position[0] > thing.position[0]) thing.position[0]++;
+    if (leadingThing.position[0] < thing.position[0]) thing.position[0]--;
+    if (leadingThing.position[1] > thing.position[1]) thing.position[1]++;
+    if (leadingThing.position[1] < thing.position[1]) thing.position[1]--;
+
+    if(tailNotVisited(thing)) thing.visited.push([thing.position[0], thing.position[1]]);
+    return;
+  }
+}
+
+function moveThingRightLeft(leadingThing, thing, plusMinus) {
+  if (Math.abs(thing.position[0] - leadingThing.position[0]) >= 2) {
+    if (leadingThing.position[0] > thing.position[0]) thing.position[0]++;
+    if (leadingThing.position[0] < thing.position[0]) thing.position[0]--;
+    if (leadingThing.position[1] > thing.position[1]) thing.position[1]++;
+    if (leadingThing.position[1] < thing.position[1]) thing.position[1]--;
+
+    if(tailNotVisited(thing)) thing.visited.push([thing.position[0], thing.position[1]]);
+    return;
+  }
+
+  if (Math.abs(thing.position[1] - leadingThing.position[1]) >= 2) {
+    if (leadingThing.position[0] > thing.position[0]) thing.position[0]++;
+    if (leadingThing.position[0] < thing.position[0]) thing.position[0]--;
+    if (leadingThing.position[1] > thing.position[1]) thing.position[1]++;
+    if (leadingThing.position[1] < thing.position[1]) thing.position[1]--;
+
+    if(tailNotVisited(thing)) thing.visited.push([thing.position[0], thing.position[1]]);
+    return;
+  }
+}
+
+function headMoveUp(distance, head, tail, knots) {
   for (let i=0; i < distance; i++) {
     head.position[1]++;
-    if (sameColumn(tail.position, head.position) && Math.abs(tail.position[1] - head.position[1]) === 2) {
-      tail.position[1]++;
-      if(tailNotVisited(tail)) tail.visited.push([tail.position[0], tail.position[1]]);
-    }
-    if (!sameColumn(tail.position, head.position) && Math.abs(tail.position[1] - head.position[1]) === 2) {
-      tail.position = [head.position[0], head.position[1] -1];
-      if(tailNotVisited(tail)) tail.visited.push([tail.position[0], tail.position[1]]);
+    moveThingUpDown(head, tail, 1);
+
+    for (const [index, knot] of knots.entries()) {
+      if (index === 0 ) {
+        moveThingUpDown(head, knot, 1);
+      } else {
+        moveThingUpDown(knots[index - 1], knot, 1)
+      }
     }
   }
 }
 
-function headMoveDown(distance, head, tail) {
+function headMoveDown(distance, head, tail, knots) {
   for (let i=0; i < distance; i++) {
     head.position[1]--;
-    if(sameColumn(tail.position, head.position) && Math.abs(tail.position[1] - head.position[1]) === 2) {
-      tail.position[1]--;
-      if(tailNotVisited(tail)) tail.visited.push([tail.position[0], tail.position[1]]);
-    }
-    if (!sameColumn(tail.position, head.position) && Math.abs(tail.position[1] - head.position[1]) === 2) {
-      tail.position = [head.position[0], head.position[1] +1];
-      if(tailNotVisited(tail)) tail.visited.push([tail.position[0], tail.position[1]]);
+    moveThingUpDown(head, tail, -1);
+
+    for (const [index, knot] of knots.entries()) {
+      if (index === 0 ) {
+        moveThingUpDown(head, knot, -1);
+      } else {
+        moveThingUpDown(knots[index - 1], knot, -1)
+      }
     }
   }
 }
 
-function headMoveRight(distance, head, tail) {
+function headMoveRight(distance, head, tail, knots) {
   for (let i=0; i < distance; i++) {
     head.position[0]++;
-    if (sameRow(tail.position, head.position) && Math.abs(tail.position[0] - head.position[0]) === 2) {
-      tail.position[0]++;
-      if(tailNotVisited(tail)) tail.visited.push([tail.position[0], tail.position[1]]);
-    }
-    if (!sameRow(tail.position, head.position) && Math.abs(tail.position[0] - head.position[0]) === 2) {
-      tail.position = [head.position[0] -1, head.position[1]];
-      if(tailNotVisited(tail)) tail.visited.push([tail.position[0], tail.position[1]]);
+    moveThingRightLeft(head, tail, 1)
+
+    for (const [index, knot] of knots.entries()) {
+      if (index === 0 ) {
+        moveThingRightLeft(head, knot, 1);
+      } else {
+        moveThingRightLeft(knots[index - 1], knot, 1)
+      }
     }
   }
 }
 
-function headMoveLeft(distance, head, tail) {
+function headMoveLeft(distance, head, tail, knots) {
   for (let i=0; i < distance; i++) {
     head.position[0]--;
-    if (tail.position[1] === head.position[1] && Math.abs(tail.position[0] - head.position[0]) === 2) {
-      tail.position[0]--;
-      if(tailNotVisited(tail)) tail.visited.push([tail.position[0], tail.position[1]]);
-    }
-    if (tail.position[1] !== head.position[1] && Math.abs(tail.position[0] - head.position[0]) === 2) {
-      tail.position = [head.position[0] +1, head.position[1]];
-      if(tailNotVisited(tail)) tail.visited.push([tail.position[0], tail.position[1]]);
+    moveThingRightLeft(head, tail, -1)
+
+    for (const [index, knot] of knots.entries()) {
+      if (index === 0 ) {
+        moveThingRightLeft(head, knot, -1);
+      } else {
+        moveThingRightLeft(knots[index - 1], knot, -1)
+      }
     }
   }
 }
